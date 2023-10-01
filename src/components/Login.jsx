d88d7b453c51c70af3bf19cf456429f02c286a47
Sahil Rai <Raisahil580@gmail.com>
2023-10-01 01:49:31 -0700

    Log in added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css"; // Import a CSS file for styling
 
 import {
@@ -13,15 +13,24 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Login = ({ onLogin, onLogout }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    // Check if the user is already logged in when the component mounts
+    const isAuthenticated = sessionStorage.getItem("isLoggedIn") === "true";
+    if (isAuthenticated) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const handleLogin = () => {
     // Hardcoded credentials (for demonstration purposes)
-    const hardcodedUsername = "risings";
     const hardcodedPassword = "risings123!";
 
-    if (username === hardcodedUsername && password === hardcodedPassword) {
+    if (password === hardcodedPassword) {
+      // Set authentication status in session storage
+      sessionStorage.setItem("isLoggedIn", "true");
+
       setIsLoggedIn(true);
       onLogin();
     } else {
@@ -30,19 +39,23 @@ const Login = ({ onLogin, onLogout }) => {
   };
 
   const handleLogout = () => {
+    // Remove authentication status from session storage
+    sessionStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
     onLogout();
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleLogin();
+    }
+  };
+
   return (
     <div>
-        {!isLoggedIn ? (
-          <ChakraProvider>
-          <VStack
-            minHeight="100vh"
-            justifyContent="center"
-            spacing={6}
-          >
+      {!isLoggedIn ? (
+        <ChakraProvider>
+          <VStack minHeight="100vh" justifyContent="center" spacing={6}>
             <Router>
               <Link to="/">
                 <Text
@@ -51,55 +64,50 @@ const Login = ({ onLogin, onLogout }) => {
                   fontFamily="Playfair Display"
                   zIndex={5}
                   cursor="pointer"
+                  marginTop="-100px" // Move the text slightly towards the top
                 >
                   Risings GPT
                 </Text>
               </Link>
             </Router>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            spacing={6} // Add spacing between children
-            alignContent="center"
-          >
-            <Input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="input-field"
-              width="130%" // Decrease the width of the textboxes
-              marginBottom="10px" // Add more spacing below the textbox
-              marginTop="20px" // Add spacing above the textbox
-            />
-            <Input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="input-field"
-              width="130%" // Decrease the width of the textboxes
-              marginBottom="30px" // Add more spacing below the textbox
-              marginTop="20px" // Add spacing above the textbox
-            />
-            <Button
-              colorScheme="teal"
-              size="md"
-              onClick={handleLogin}
-              className="login-button"
-              width="130%" // Decrease the width of the button
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              spacing={6} // Add spacing between children
+              alignContent="center"
+              marginTop="10px" // Move the box containing inputs and button towards the top
             >
-              Login
-            </Button>
-          </Box>
+              <Input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input-field"
+                width="130%" // Decrease the width of the textboxes
+                marginBottom="30px" // Add more spacing below the textbox
+                marginTop="10px" // Slightly move the textbox towards the top
+                onKeyPress={handleKeyPress} // Handle Enter key press
+              />
+              <Button
+                colorScheme="teal"
+                size="md"
+                onClick={handleLogin}
+                className="login-button"
+                width="130%" // Decrease the width of the button
+                marginTop="10px" // Slightly move the button towards the top
+              >
+                Login
+              </Button>
+            </Box>
           </VStack>
-          </ChakraProvider>
-        ) : (
-          <div></div>
-        )}
+        </ChakraProvider>
+      ) : (
+        <div>
         </div>
+      )}
+    </div>
   );
 };
 
